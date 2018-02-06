ab238f974b38ca522d9c5e6705399faa3cf4c5bb
CubeYogi <34396887+CubeYogi@users.noreply.github.com>
2018-02-06 15:40:29 +0530

    Update deluge_script_subform_insert_records.js

--- deluge_script_subform_insert_records.js
+++ deluge_script_subform_insert_records.js
@@ -1,7 +1,14 @@
 /**
-@summary: Insert Zoho Creator subform records through Deluge script
-		  Example: Lets take a Order Management Appication
-		  Step 1: Order Form has to make one order has allow to enter multiple products records which is from the 'Product' Form
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+WHAT IS THIS SCRIPT FOR?
+------------------------
+Inserting Zoho Creator subform records through Deluge script.**
+Why so much talk about this? Well, this question has been asked in 2013 in Zoho Forum but no answer yet, with about 40 people asking the same feature.
+https://help.zoho.com/portal/community/topic/how-to-add-row-to-subform-via-script
+So we are here to offer a solution to this. Here's an example of how to achieve this.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	Example: Let's consider an Order Form, for making entries into an order 
+	Step 1: Order Form has to make one order has allow to enter multiple products records which is from the 'Product' Form
 		  Step 2: So In 'Order Form' has the Subform for 'Product Form' which is allow to enter multiple product records for one order
 		  Step 3: In such case to insert the product records for the single order througth the deluge script
 		  Step 4: Add lookup field in 'Product Form' which is from the 'Order Form' unique id
@@ -9,11 +16,6 @@
 		  Step 6: Using the Order Form unique ID to insert the 'Product Form' records to 'Product Form'
 		  Step 7: Fetch records in 'Product Form' with the Order Form unique id to get the 'Product Form' collection records
 		  Step 8. Now Update the 'Order Form's Product subform field to collection records of the 'Product Form' 
-
-@author: Sarathi, iKosmik.
-@created: 02 Feb 2018.
-@copyright: iKosmik Software Pvt Ltd., 2018
-
 */
 
 /** Deluge script for Order Entry **/
